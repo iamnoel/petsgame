d9c7c6331c13199ef590e5b0febfa87794210d16
Noel R Ward <noel1@Noels-MacBook-Pro.local>
2020-02-09 14:49:35 -0500

    Initial file upload

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+require('dotenv').config();
+
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Application listening on port ${port}`);
+});
+
+app.get('/', (req, res) => res.send('Hello pets!'));
