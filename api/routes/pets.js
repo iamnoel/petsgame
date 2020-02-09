ae0f65f56fcbf6c5c9c40466be732f11124697bb
Noel <noel.ward@icloud.com>
2020-02-09 18:47:15 -0500

    added pets api route

--- api/routes/pets.js
+++ api/routes/pets.js
@@ -13,4 +13,20 @@ router.post('/', (req, res, next) => {
   });
 });
 
+router.get('/:petID', (req, res, next) => {
+  const id = req.params.petID;
+
+  if (id == '0012') {
+    res.status(200).json({
+      message: `The unique pet ID ${id} was found!`,
+      id: id,
+    });
+  } else {
+    res.status(200).json({
+      message: `You entered id ${id}`,
+      id: id,
+    });
+  }
+});
+
 module.exports = router;
