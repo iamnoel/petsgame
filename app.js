573d905e15e59b42d87713a81640e9071b543fdf
Noel <noel.ward@icloud.com>
2020-02-09 23:44:17 -0500

    added CORS handling

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // Routes for request handling
 app.use('/pets/', petRoutes);
 app.use('/users/', userRoutes);
