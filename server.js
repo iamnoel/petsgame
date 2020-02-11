750432518ad7e5e453b1acf2a219301de1199ebb
Noel <noel.ward@icloud.com>
2020-02-11 18:46:13 -0500

    migrated api backend code to another project

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const http = require('http');
-const app = require('./app');
-
-const port = process.env.PORT || 3001;
-
-const server = http.createServer(app);
-
-server.listen(port);
